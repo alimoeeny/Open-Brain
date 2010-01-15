464911bfd52e78f4057aff9b45e4dcb68a054ae1
Ali Moeeny <alimoeeny@gmail.com>
2010-01-15 14:06:23 -0500

    just another update

--- openbrain.py
+++ openbrain.py
@@ -1,5 +1,5 @@
 
-ExperimentType = set('cylinder.ABD', 'rds.OT')
+ExperimentType = set(['cylinder.ABD', 'rds.OT'])
 
 
 class OBDBO(object):
@@ -12,14 +12,15 @@ class Monkey(OBDBO):
         self.name = name
 
 class Experiment(OBDBO):
-    def __init__(self, name, experimentType):
+    def __init__(self, name, experimentType, monkeyname):
         self.name = name
+        self.monkeyName = monkeyname
         self.experimentType = experimentType
 
 class Neuron(OBDBO):
     def __init__(self, name, experimentname):
         self.name = name
-        self.experimentName = experiment
+        self.experimentName = experimentname
 
 class Trial(OBDBO):
     pass
@@ -43,14 +44,12 @@ class OpenBrain(object):
         f = open(dataPath + 'Experiments.txt')
         allLines = f.readlines()
         for ee in allLines:
-            self.Experiments.append(Experiment(ee.split()[0], ee.split()[1]))
+            self.Experiments.append(Experiment(ee.split()[0], ee.split()[1], ee.split()[2]))
         f.close()
 
         self.Neurons = [];
         f = open(dataPath + 'Neurons.txt')
         allLines = f.readlines()
-        for nn in allLines:
-            mnk = 
+        for nn in allLines: 
             self.Neurons.append(Neuron(nn.split()[0], nn.split()[1]))
         f.close()
-
