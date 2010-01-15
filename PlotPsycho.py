521b609d1eb76f23f438161b9887ee31b1645e1b
Ali Moeeny <alimoeeny@gmail.com>
2010-01-15 10:52:38 -0500

    Yet another update, started adding classes to openbrain.py work goes on hopefuly

--- PlotPsycho.py
+++ PlotPsycho.py
@@ -6,7 +6,8 @@ def PlotPsycho(filename):
      Expt, ExptHeader, ExptStimvals, ExptTrials = BFileImport(filename)
      
      dxs = unique(ExptTrials['dx'])
-     perfo = zeros((dxs.size, 2))
+     for i in range(0,dxs.size): dxs[i] = dxs[i][0][0]
+     perfo = zeros((dxs.size, 3))
 
      if (ExptTrials[ExptTrials['dx']>0]['RespDir'].mean()>0):
          ResponseToPositive = 1;
@@ -18,9 +19,11 @@ def PlotPsycho(filename):
      for i in range(0, dxs.size):
          perfo[i,0] = sum(ExptTrials[ExptTrials['dx']==dxs[i].flat]['RespDir']==ResponseToPositive)
          perfo[i,1] = sum(ExptTrials[ExptTrials['dx']==dxs[i].flat]['RespDir']!=0)
-                  
+         perfo[i,2] = sqrt(perfo[i,0] * (1 - perfo[i,0] / perfo[i,1]))
+
      figure(),
-     plot(dxs, perfo[:,0])
+     #plot(dxs, perfo[:,0])
+     errorbar(dxs, perfo[:,0], yerr=perfo[:,2], marker='o', mfc='red', mec='red', ms=10, mew=2)
      #figimage(rpi)
      show()
 
--- openbrain.py
+++ openbrain.py
@@ -1,1 +1,28 @@
 
+class OBDBO(object):
+    """ Open Brain Data Base Object base """
+    pass
+
+class Monkey(OBDBO):
+    pass
+
+class Neuron(OBDBO):
+    pass
+
+class Experiment(OBDBO):
+    pass
+
+class ExperimentType(OBDBO):
+    pass
+
+class Trial(OBDBO):
+    pass
+
+class SpikeTrain(OBDBO):
+    pass
+
+class Stimulus(OBDBO):
+    pass
+
+
+
