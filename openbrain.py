a448a268438a76fa7affb7d345583dcfb828921e
Ali Moeeny <alimoeeny@gmail.com>
2010-01-15 16:12:32 -0500

    Just to please myself I am committing!

--- openbrain.py
+++ openbrain.py
@@ -1,3 +1,4 @@
+from BFileImport import BFileImport
 
 ExperimentType = set(['cylinder.ABD', 'rds.OT'])
 
@@ -8,17 +9,25 @@ class OBDBO(object):
         pass
     
 class Monkey(OBDBO):
-    def __init__(self, name):
+    def __init__(self, name, parent):
+        self.parent = parent
         self.name = name
 
 class Experiment(OBDBO):
-    def __init__(self, name, experimentType, monkeyname):
+    def __init__(self, name, monkeyname, experimentType, parent):
+        self.parent = parent
         self.name = name
         self.monkeyName = monkeyname
         self.experimentType = experimentType
 
+    def loadData(self):
+        filename = self.parent.spikeDataPath + self.name[3:6] + '/' +  self.name
+        print filename 
+        a, b, c, d = BFileImport(filename)
+
 class Neuron(OBDBO):
-    def __init__(self, name, experimentname):
+    def __init__(self, name, experimentname, parent):
+        self.parent = parent
         self.name = name
         self.experimentName = experimentname
 
@@ -32,24 +41,25 @@ class Stimulus(OBDBO):
     pass
 
 class OpenBrain(object):
-    def __init__(self, dataPath='./'):
+    def __init__(self, dataPath='./', spikeDataPath='/bgc/data/dae/'):
+        self.spikeDataPath = spikeDataPath
         f = open(dataPath + 'Monkeys.txt')
         allLines = f.readlines()
         self.Monkeys = [];
         for ll in allLines:
-            self.Monkeys.append(Monkey(ll[0:-1]))
+            self.Monkeys.append(Monkey(ll[0:-1], self))
         f.close()
 
         self.Experiments = [];
         f = open(dataPath + 'Experiments.txt')
         allLines = f.readlines()
         for ee in allLines:
-            self.Experiments.append(Experiment(ee.split()[0], ee.split()[1], ee.split()[2]))
+            self.Experiments.append(Experiment(ee.split()[0], ee.split()[1], ee.split()[2], self))
         f.close()
 
         self.Neurons = [];
         f = open(dataPath + 'Neurons.txt')
         allLines = f.readlines()
         for nn in allLines: 
-            self.Neurons.append(Neuron(nn.split()[0], nn.split()[1]))
+            self.Neurons.append(Neuron(nn.split()[0], nn.split()[1], self))
         f.close()
