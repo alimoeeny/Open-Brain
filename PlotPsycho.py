635573d28c65085b370222850f6a4b9ebc3c11ad
Ali Moeeny <alimoeeny@gmail.com>
2010-01-17 10:13:59 -0500

    PlotPsycho can plot separate blocks in an experiment separatly.

--- PlotPsycho.py
+++ PlotPsycho.py
@@ -2,64 +2,73 @@ from BFileImport import *
 from numpy import *
 from pylab import *
 
-
-def PlotPsycho(filename):
-     Expt, ExptHeader, ExptStimvals, ExptTrials = BFileImport(filename)
-     
-     dxs = unique(ExptTrials['dx'])
-     for i in range(0,dxs.size): dxs[i] = dxs[i][0][0]
-     perfo = zeros((dxs.size, 3))
-
-     if (ExptTrials[ExptTrials['dx']>0]['RespDir'].mean()>0):
-         ResponseToPositive = 1;
-         ResponseToNegative = -1;
-     else:
-         ResponseToPositive = -1;
-         ResponseToNegative = 1;
-
-     for i in range(0, dxs.size):
-         perfo[i,0] = sum(ExptTrials[ExptTrials['dx']==dxs[i].flat]['RespDir']==ResponseToPositive)
-         perfo[i,1] = sum(ExptTrials[ExptTrials['dx']==dxs[i].flat]['RespDir']!=0)
-         perfo[i,2] = sqrt(perfo[i,0] * (1 - perfo[i,0] / perfo[i,1]))
-
-     figure(),
-     #plot(dxs, perfo[:,0])
-     errorbar(dxs, perfo[:,0], yerr=perfo[:,2], marker='o', mfc='red', mec='red', ms=10, mew=2)
-     #figimage(rpi)
-     show()
-
-
 def PlotPsycho(experiments, StimulusVariable, MergeBlocks=True):
      figure(),
      for expt in experiments:
-          dxs = expt.getValuesFor(StimulusVariable)
-          perfo = zeros((dxs.__len__(), 3))
+          if MergeBlocks:
+               dxs = expt.getValuesFor(StimulusVariable)
+               perfo = zeros((dxs.__len__(), 3))
 
-          seltrials = expt.getTrialsWith(StimulusVariable, 0, "greaterthan")
-          s = 0
-          for t in array(expt.Trials)[seltrials]: 
-               s = s + t.RespDir
-          if (s / seltrials.__len__()>0):
-               ResponseToPositive = 1;
-               ResponseToNegative = -1;
-          else:
-               ResponseToPositive = -1;
-               ResponseToNegative = 1;
-
-          for i in range(0, dxs.__len__()):
-               seltrials = expt.getTrialsWith(StimulusVariable, dxs[i])
+               seltrials = expt.getTrialsWith(StimulusVariable, 0, "greaterthan")
                s = 0
                for t in array(expt.Trials)[seltrials]: 
-                    if t.RespDir==ResponseToPositive: 
-                         s = s + 1
-               perfo[i,0] = s #sum(array(expt.Trials)[seltrials]['RespDir']==ResponseToPositive)
-               s = 0
-               for t in array(expt.Trials)[seltrials]: 
-                    if t.RespDir!=0: 
-                         s = s + 1
-               perfo[i,1] = s #sum(array(expt.Trials)[seltrials]['RespDir']!=0)
-               perfo[i,2] = sqrt(perfo[i,0] * (1 - perfo[i,0] / perfo[i,1]))
-          print dxs
-          errorbar(dxs, perfo[:,0], yerr=perfo[:,2], marker='o', mfc='red', mec='red', ms=10, mew=2)
+                    s = s + t.RespDir
+               if (s / seltrials.__len__()>0):
+                    ResponseToPositive = 1;
+                    ResponseToNegative = -1;
+               else:
+                    ResponseToPositive = -1;
+                    ResponseToNegative = 1;
+
+               for i in range(0, dxs.__len__()):
+                    seltrials = expt.getTrialsWith(StimulusVariable, dxs[i])
+                    s = 0
+                    for t in array(expt.Trials)[seltrials]: 
+                         if t.RespDir==ResponseToPositive: 
+                              s = s + 1
+                    perfo[i,0] = s #sum(array(expt.Trials)[seltrials]['RespDir']==ResponseToPositive)
+                    s = 0
+                    for t in array(expt.Trials)[seltrials]: 
+                         if t.RespDir!=0: 
+                              s = s + 1
+                    perfo[i,1] = s #sum(array(expt.Trials)[seltrials]['RespDir']!=0)
+                    perfo[i,2] = sqrt(perfo[i,0] * (1 - perfo[i,0] / perfo[i,1]))
+               print dxs
+               errorbar(dxs, perfo[:,0], yerr=perfo[:,2], marker='o', mfc='red', mec='red', ms=10, mew=2)
+          else: # if !MergeBlocks
+               for blk in expt.BlockStart[0]:
+                    blockTrials = expt.getTrialsWith('block', blk)
+                    dxs = expt.getValuesFor(StimulusVariable, blockTrials)
+                    if ((blockTrials.__len__() < dxs.__len__() * 5) or (dxs==[])):
+                         print 'NOT Enough trials in this block (' + blockTrials.__len__().__str__() + ' trials)'
+                    else:
+                         perfo = zeros((dxs.__len__(), 3))
+                         seltrials = expt.getTrialsWith(StimulusVariable, 0, "greaterthan")
+                         s = 0
+                         for t in array(expt.Trials)[seltrials]: 
+                              s = s + t.RespDir
+                         if (s / seltrials.__len__()>0):
+                              ResponseToPositive = 1;
+                              ResponseToNegative = -1;
+                         else:
+                              ResponseToPositive = -1;
+                              ResponseToNegative = 1;
+
+                         for i in range(0, dxs.__len__()):
+                              seltrials = expt.getTrialsWith(StimulusVariable, dxs[i])
+                              s = 0
+                              for t in array(expt.Trials)[seltrials]: 
+                                   if t.RespDir==ResponseToPositive: 
+                                        s = s + 1
+                              perfo[i,0] = s #sum(array(expt.Trials)[seltrials]['RespDir']==ResponseToPositive)
+                              s = 0
+                              for t in array(expt.Trials)[seltrials]: 
+                                   if t.RespDir!=0: 
+                                        s = s + 1
+                              perfo[i,1] = s #sum(array(expt.Trials)[seltrials]['RespDir']!=0)
+                              perfo[i,2] = sqrt(perfo[i,0] * (1 - perfo[i,0] / perfo[i,1]))
+                         print dxs.__str__() + blockTrials.__len__().__str__() 
+                         errorbar(dxs, perfo[:,0], yerr=perfo[:,2], marker='o', mfc='red', mec='red', ms=10, mew=2)
+
      show()
 
--- openbrain.py
+++ openbrain.py
@@ -25,6 +25,28 @@ class Experiment(OBDBO):
         filename = self.parent.spikeDataPath + self.name[3:6] + '/' +  self.name
         print filename 
         a, b, c, d = BFileImport(filename)
+ 
+        for sv in c.dtype.names:
+            if sv in set(['or', 'and', 'if', 'in', 'is']):
+                exec("self." + sv.upper() + " = squeeze(c['" + sv + "']) + 0.0")
+            else:
+                try:
+                    exec("self." + sv + " = squeeze(c['" + sv + "']) + 0.0")
+                except:
+                    exec("self." + sv + " = squeeze(c['" + sv + "'])")
+        for sv in b.dtype.names:
+            #print sv
+            if sv in set(['or', 'and', 'if', 'in', 'is', 'Name']):
+                try:
+                    exec("self." + sv.upper() + " = squeeze(b['" + sv + "']) + 0.0")
+                except:
+                    exec("self." + sv.upper() + " = squeeze(b['" + sv + "'])")
+            else:
+                try:
+                    exec("self." + sv + " = squeeze(b['" + sv + "']) + 0.0")
+                except:
+                    exec("self." + sv + " = squeeze(b['" + sv + "'])")
+
         self.Trials = []
         for tt in d:
             self.Trials.append(Trial(self)) 
@@ -34,7 +56,8 @@ class Experiment(OBDBO):
                 except:
                     exec("self.Trials[-1]." + dtn + " = squeeze(tt['" + dtn + "'])")
                 
-        print 'Got ' + self.Trials.__len__().__str__() + ' trials!'
+        print 'Got ' + self.Trials.__len__().__str__() + ' trials! in ' + self.BlockStart[0].__len__().__str__() + ' Blocks'
+
 
     def TrialDuration(self):
         durations = []
@@ -42,26 +65,38 @@ class Experiment(OBDBO):
             durations = self.Trials[i].dur
         return median(array(durations))
     
-    def getValuesFor(self, CategorizeBy):
+    def getValuesFor(self, CategorizeBy, selectTrials=None):
         values = set([])
-        for i in range(0, self.Trials.__len__()):
-            exec('values.add(self.Trials[' + i.__str__() + '].' + CategorizeBy + ')')
+        if selectTrials==None:
+            selectTrials = range(0,self.Trials.__len__())
+        for i in range(0, selectTrials.__len__()):
+            exec('values.add(self.Trials[' + selectTrials[i].__str__() + '].' + CategorizeBy + ')')
         a = list(values)
         a.sort()
         return a
 
     def getTrialsWith(self, CategorizeBy, catValue, Operator=None):
         r = []
-        for i in range(0, self.Trials.__len__()):
-            if Operator==None:
-                if eval('self.Trials[' + i.__str__() + '].' + CategorizeBy ) == catValue :
+        if (CategorizeBy.lower() == 'block'):
+            blockStart = catValue
+            if list(self.BlockStart[0]).index(catValue) +1 < self.BlockStart[0].__len__():
+                blockEnd = self.BlockStart[0][list(self.BlockStart[0]).index(catValue)+1]
+            else:
+                blockEnd = self.Trials[-1].Trial + 1
+            for i in range(0, self.Trials.__len__()):
+                if ( self.Trials[i].Trial >=blockStart and self.Trials[i].Trial < blockEnd):
                     r.append(i)
-            elif Operator=="greaterthan":
-                if eval('self.Trials[' + i.__str__() + '].' + CategorizeBy ) > catValue :
-                    r.append(i)
-            elif Operator=="lessthan":
-                if eval('self.Trials[' + i.__str__() + '].' + CategorizeBy ) < catValue :
-                    r.append(i)
+        else:
+            for i in range(0, self.Trials.__len__()):
+                if Operator==None:
+                    if eval('self.Trials[' + i.__str__() + '].' + CategorizeBy ) == catValue :
+                        r.append(i)
+                elif Operator=="greaterthan":
+                    if eval('self.Trials[' + i.__str__() + '].' + CategorizeBy ) > catValue :
+                        r.append(i)
+                elif Operator=="lessthan":
+                    if eval('self.Trials[' + i.__str__() + '].' + CategorizeBy ) < catValue :
+                        r.append(i)
         return r
 
 class Neuron(OBDBO):
